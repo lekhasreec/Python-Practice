281e5fe1c1b1eff488d2c090eed98a6b074c954c
Lekhasree C <lekhasreec295@gmail.com>
2026-09-08 13:11:31 +0530

    Add files via upload

---
+++ Day-02/arithmetic.py
@@ -0,0 +1,8 @@
+#Take two numbers and print:Addition,Subtraction,Multiplication,Division
+a,b=map(int,input("Enter any two numbers :").split())
+print("Addition of two numbers is :",a+b)
+print("Subtraction of two numbers is :",a-b)
+print("Multiplication of two numbers is :",a*b)
+print("Division of two numbers is :",a/b)
+print("Modulus of two numbers is :",a%b)
+print("Floor Division of two numbers is :",a//b)
---
+++ Day-02/square.py
@@ -0,0 +1,3 @@
+#Take a number and print its square.
+a=int(input("Enter a number :"))
+print("The square of the number is :",a*a)
