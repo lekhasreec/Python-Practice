339f371f5ac12650f1bd90a344827e2b99c38a88
Lekhasree C <lekhasreec295@gmail.com>
2026-09-08 13:04:26 +0530

    Delete Largestnumber.py

--- Largestnumber.py
+++
@@ -1,7 +0,0 @@
-#Write a program to take two numbers and print the largest number.
-num1=int(input("Enter 1st number:"))
-num2=int(input("Enter 2nd number:"))
-if num1>num2:
-    print("The Largest Number is :",num1)
-else:
-    print("The Largest Number is :",num2)
