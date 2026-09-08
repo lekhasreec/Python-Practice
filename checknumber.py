0c3ff418df4e1fe0e8cc97a114dc39c39d524be5
Lekhasree C <lekhasreec295@gmail.com>
2026-09-08 13:07:04 +0530

    Delete checknumber.py

--- checknumber.py
+++
@@ -1,8 +0,0 @@
-#Write a program to check whether a number is positive, negative, or zero.
-num=int(input("Enter a number:"))
-if num>0:
-    print("The Given number is Positive")
-elif num==0:
-    print("The Given Number is Zero")
-else:
-    print("The Given Number is Negative")
