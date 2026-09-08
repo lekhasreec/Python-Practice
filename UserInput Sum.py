26043ae0f41e9098475d09af6f8cd91f2a05cba3
Lekhasree C <lekhasreec295@gmail.com>
2026-09-08 13:06:50 +0530

    Delete UserInput Sum.py

--- UserInput Sum.py
+++
@@ -1,5 +0,0 @@
-#Write a Python program to take two numbers as input and print their sum.
-num1=int(input("Enter 1st number:"))
-num2=int(input("Enter 2nd number:"))
-sum=num1+num2
-print("The Sum of the two numbers is :",sum)
